6589d54e0ee5a55c2c747428ebe6dc1ba4b3113d
luca <luca.scolletta@outlook.it>
2025-02-05 19:51:59 +0100

    Terminata lezione 8

--- Lezione9/prova.c
+++ Lezione9/prova.c
@@ -5,43 +5,62 @@
 
 #define MAX_NOME 21
 #define MAX_TENTATIVI 3
+#define MAX_PARTITE 100
 
 typedef struct {
     char nome[MAX_NOME];
     int punteggio;
-} Risultato;
+}Risultato;
 
 int generaNumeroCasuale(int min, int max);
-
 int calcolaPunteggio(int tentativi[], int numeroSegreto);
-
-void registraRisultato(int punteggio, char* nome, Risultato* partite, int* dim) ;
-    
-void gioco(int numeroSegreto, int tentativi[]);
-
+void registraRisultato(int punteggio, char* nome, Risultato* partite, int* dim);
+void gioco(int numeroSegreto, int tentativi[], Risultato* partite, int* dim);
 void stampaPartite(Risultato* partite, int dim);
 
 int main() {
+    int numeroSegreto = generaNumeroCasuale(0, 100);
+    int* tentativi = calloc(MAX_TENTATIVI, sizeof(int));
+    Risultato* partite = calloc(MAX_PARTITE, sizeof(Risultato));
+    int dim = 0;
 
+    gioco(numeroSegreto, tentativi, partite, &dim);
+}
+
+int generaNumeroCasuale(int min, int max) {
+    return rand() % (max - min + 1) + min;
 }
 
 int calcolaPunteggio(int tentativi[], int numeroSegreto) {
-    int distanza = abs(tentativi[0] - numeroSegreto);
+    int distanzaMinima = abs(tentativi[0] - numeroSegreto);
 
+    int i = 1;
     do {
+        if(abs(tentativi[i] - numeroSegreto) < distanzaMinima) {
+            distanzaMinima = abs(tentativi[i] - numeroSegreto);
+        }      
+    } while(i < MAX_TENTATIVI && tentativi[i] != numeroSegreto);
 
-    } while();
+    return 100 - distanzaMinima;
 }
 
-// [60, 87, ]
+void registraRisultato(int punteggio, char* nome, Risultato* partite, int* dim) {
+    Risultato nuovoRisultato;
+    nuovoRisultato.punteggio = punteggio;
+    strcpy(nuovoRisultato.nome, nome);
 
+    partite[*dim] = nuovoRisultato;
+    
+    (*dim)++;
+}
 
-void gioco(int numeroSegreto, int tentativi[]) {
+void gioco(int numeroSegreto, int tentativi[], Risultato* partite, int* dim) {
     int numeroTentativi = 0;
 
     do {
         printf("Inserisci il %d tentativo:\n", numeroTentativi + 1);
         scanf("%d", &tentativi[numeroTentativi]);
+        printf("Hai inserito %d\n", tentativi[numeroTentativi]);
 
         if(tentativi[numeroTentativi] == numeroSegreto) {
             printf("Complimenti! Hai indovinato! Hai utilizzato %d tentativi\n", numeroTentativi + 1);
@@ -58,21 +77,24 @@ void gioco(int numeroSegreto, int tentativi[]) {
         numeroTentativi++;
     } while(numeroTentativi < MAX_TENTATIVI && tentativi[numeroTentativi] != numeroSegreto);
 
-    //int punteggio = calcolaPunteggio(tentativi, numeroSegreto);
+    printf("ciao");
+    int punteggio = calcolaPunteggio(tentativi, numeroSegreto);
 
     char nome[MAX_NOME];
 
     printf("Inserisci nome:\n");
     scanf("%[^\n]", nome);
 
-
-    //registraRisultato();
+    registraRisultato(punteggio, nome, partite, dim);
 }
 
-
-
-int generaNumeroCasuale(int min, int max) {
-    return rand() % (max - min + 1) + min;
+void stampaPartite(Risultato* partite, int dim) {
+    printf("----------PUNTEGGI----------");
+    for(int i = 0; i < dim; i++) {
+        printf("%s %d\n", partite[i].nome, partite[i].punteggio);
+    }
+    printf("----------------------------");
 }
+
 
 
