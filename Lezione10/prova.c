9e1b3ed238eba283aeb9314d4e753ccf679aeb7a
luca <luca.scolletta@outlook.it>
2025-02-12 19:16:17 +0100

    Aggiunta lezione 10

--- Lezione10/prova.c
+++ Lezione10/prova.c
@@ -42,6 +42,79 @@ bool registra_uscita(Auto* lista_auto, int posizione, int ora_uscita, float* inc
 void stampa(Auto* lista_auto, float incasso);
 
 int main() {
+    Auto lista_auto[MAX_POSTI_AUTO];
+    float incasso = 0;
+
+    for(int i = 0; i < MAX_POSTI_AUTO; i++) {
+        lista_auto[i].libero = true;
+    }
+
+    int risposta;
+
+    do {
+        printf("1 - Registra un nuovo ingresso\n2 - Registra un'uscita\n3 - Chiudi registro e visualizza report finale\n");
+        scanf("%d", &risposta);
+
+        switch(risposta) {
+            case 1: {
+                int posizione_libera = registra_ingresso(lista_auto);
+                if(posizione_libera == MAX_POSTI_AUTO) {
+                    printf("ERRORE: non ci sono posti liberi!\n");
+                }
+                else {
+                    printf("Il %d posto è libero\n", posizione_libera + 1);
+                    printf("Inserisci ora di ingresso:\n");
+                    scanf("%d", &lista_auto[posizione_libera].ora_ingresso);
+                    lista_auto[posizione_libera].libero = false;
+                }
+                break;
+            }
+
+            case 2: {
+                int posizione;
+                int ora_uscita;
+
+                do {
+                    printf("Inserisci la posizione:\n");
+                    scanf("%d", &posizione);
+
+                    if(posizione > 10 || posizione < 1) {
+                        printf("ERRORE: posizione sbagliata\n");
+                    }
+                } while(posizione > 10 || posizione < 1);
+            
+                do {
+                    printf("Inserisci ora di uscita (da 0 a 23):\n");
+                    scanf("%d", &ora_uscita);
+
+                    if(ora_uscita > 23 || ora_uscita < 0) {
+                        printf("ERRORE: orario scorretto\n");
+                    }
+                } while(ora_uscita > 23 || ora_uscita < 0);
+
+                bool esito = registra_uscita(lista_auto, posizione - 1, ora_uscita, &incasso);
+
+                if(esito) {
+                    printf("Incasso corrente: %.2f euro\n", incasso);
+                } 
+                else {
+                    printf("ERRORE: la posizione inserita è libera\n");
+                }
+
+                break;
+            }
+
+            case 3: {
+                stampa(lista_auto, incasso);
+                break;
+            }
+            default:  {
+                printf("ERRORE: scelta non valida\n");
+                break;
+            }
+        }   
+
+    } while(risposta != 3);
     
 }
 
@@ -50,10 +123,6 @@ int registra_ingresso(Auto* lista_auto) {
 
     for(i = 0; i < MAX_POSTI_AUTO; i++) {
         if(lista_auto[i].libero == true) {
-            printf("Il %d posto è libero\n", i + 1);
-            printf("Inserisci ora di ingresso:\n");
-            scanf("%d\n", &lista_auto[i].ora_ingresso);
-            lista_auto[i].libero = false;
             break;
         }
     }
@@ -69,8 +138,10 @@ bool registra_uscita(Auto* lista_auto, int posizione, int ora_uscita, float* inc
     // La posizione è occupata
     lista_auto[posizione].libero = true;
 
-    *incasso += (ora_uscita - lista_auto[posizione].ora_ingresso)*(TARIFFA_ORARIA);
-
+    if(ora_uscita >= lista_auto[posizione].ora_ingresso)
+        *incasso += (ora_uscita - lista_auto[posizione].ora_ingresso)*(TARIFFA_ORARIA);
+    else
+        *incasso += (24 - lista_auto[posizione].ora_ingresso + ora_uscita)*(TARIFFA_ORARIA);
     return true;
 }
 
@@ -83,6 +154,5 @@ void stampa(Auto* lista_auto, float incasso) {
             printf("La %d posizione è libera\n", i+1);
         }
     }
-
-    printf("L'incasso totale è %f euro\n", incasso);
+    printf("L'incasso totale è %.2f euro\n", incasso);
 }
